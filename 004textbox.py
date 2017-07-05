41a72907f10119b5f10ca535897a85ab3dddd27e
bnoden <brandroid369@gmail.com>
2017-07-05 03:39:13 -0500

    .

---
+++ 004textbox.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter import ttk
+
+mainwindow = tk.Tk()
+
+mainwindow.title("Textbox")
+
+label1 = ttk.Label(mainwindow, text="Label")
+label1.grid(column=0, row=0)
+
+
+def pushbutton():
+    act.configure(text="Hi " + name.get())
+
+ttk.Label(mainwindow, text="User Name:").grid(column=0, row=0)
+
+name = tk.StringVar()
+name_entered = ttk.Entry(mainwindow, width=24, textvariable=name)
+name_entered.grid(column=0, row=1)
+
+act = ttk.Button(mainwindow, text="button", command=pushbutton)
+act.grid(column=1, row=1)
+
+mainwindow.mainloop()
